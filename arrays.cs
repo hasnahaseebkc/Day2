3b1974a08279b04dcc02b9f5aef320ff59b25f8e
hasnahaseebkc <hasna.haseeb.k.c@ibm.com>
2022-03-06 18:03:13 +0530

    commit

--- arrays.cs
+++ arrays.cs
@@ -8,9 +8,42 @@ namespace MyFirstConsoleApp
 {
     internal class arrays
     {
-        public static void Main(string args[])
+        public static void Main(string[] args)
         {
+            string[] dotnet = { "C#", "ASP", "ADO", "SQL" };
+            for (int i = 0; i < dotnet.Length; i++)
+                Console.WriteLine( dotnet[i] );
+            Console.WriteLine();
+            foreach(string i in dotnet)
+                Console.WriteLine( i );
 
+            Console.WriteLine();
+            Array.Sort( dotnet );
+            foreach(string i in dotnet)
+                Console.WriteLine(i);
+
+            Console.WriteLine();
+            int[] myNumbers = { 5, 1, 8, 9 };
+            Console.WriteLine(myNumbers.Max()); //returns the largest value
+            Console.WriteLine(myNumbers.Min()); //returns the smallest value
+            Console.WriteLine(myNumbers.Sum()); //returns the sum of elements
+            Console.WriteLine();
+
+
+            //an array with 5 rows and 2 columns
+            int[,] a = new int[5, 2] { { 0, 0 }, { 1, 2 }, { 2, 4 }, { 3, 6 }, { 4, 8 } };
+            int k, l;
+            for (k = 0; k < 5; k++)
+            {
+                for (l = 0; l < 2; l++)
+                {
+                    Console.WriteLine("a[{0}, {1}] = {2}", k, l, a[k, l]);
+                }
+                    
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
